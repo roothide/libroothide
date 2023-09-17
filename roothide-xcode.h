699b75a1a44a4d0685e25ff6d7f47afe0434dcab
RootHide <134120506+RootHide@users.noreply.github.com>
2023-09-18 05:08:34 +0800

    Update roothide-xcode.h

--- roothide-xcode.h
+++ roothide-xcode.h
@@ -1,5 +1,7 @@
-#if !defined(ROOTHIDE_USE_STUB) && !defined(THEOS_PACKAGE_SCHEME_ROOTFUL) && !defined(THEOS_PACKAGE_SCHEME_ROOTLESS)
-#include "roothide/roothide.h"
-#else
+#if defined(ROOTHIDE_USE_STUB) && defined(THEOS_PACKAGE_SCHEME_ROOTHIDE)
+#error "ROOTHIDE_USE_STUB defined, but THEOS_PACKAGE_SCHEME=roothide"
+#elif defined(ROOTHIDE_USE_STUB) && !defined(THEOS_PACKAGE_SCHEME_ROOTHIDE)
 #include "roothide/stub.h"
+#else
+#include "roothide/roothide.h"
 #endif
